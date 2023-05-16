0184c2c3e8042c25cfe0c0bdcc23b45f6d0eed51
lisandrojm <lisandrojm@gmail.com>
2023-05-16 14:16:39 -0300

    Testing

--- ProductManager.js
+++ ProductManager.js
@@ -1,12 +1,13 @@
 ////////////////////////////////////////////////////////////////////////////////
 /* DESAFÍO ENTREGABLE */
-
 ////////////////////////////////////////////////////////////////////////////////
+
 const fs = require('fs');
 class ProductManager {
   constructor(path) {
     this.path = path;
   }
+
   addProduct(product) {
     const products = this.getProducts();
     const newProduct = {
@@ -21,6 +22,7 @@ class ProductManager {
     products.push(newProduct);
     this.saveProducts(products);
   }
+
   getProducts() {
     try {
       const data = fs.readFileSync(this.path, 'utf-8');
@@ -29,10 +31,12 @@ class ProductManager {
       return [];
     }
   }
+
   getProductById(id) {
     const products = this.getProducts();
     return products.find((product) => product.id === id) || null;
   }
+
   updateProduct(id, updatedFields) {
     const products = this.getProducts();
     const index = products.findIndex((product) => product.id === id);
@@ -43,6 +47,7 @@ class ProductManager {
       throw new Error(`Product with id ${id} not found.`);
     }
   }
+
   deleteProduct(id) {
     const products = this.getProducts();
     const index = products.findIndex((product) => product.id === id);
@@ -53,11 +58,49 @@ class ProductManager {
       throw new Error(`Product with id ${id} not found.`);
     }
   }
+
   getNewId(products) {
-    const productIds = products.map((product) => product.id);
-    return productIds.length > 0 ? Math.max(...productIds) + 1 : 1;
+    return products.length > 0 ? Math.max(...products.map((product) => product.id)) + 1 : 1;
   }
   saveProducts(products) {
     fs.writeFileSync(this.path, JSON.stringify(products, null, 2));
   }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+/* Testing */
+
+const productManager = new ProductManager('productos.json');
+
+console.log(productManager.getProducts());
+
+productManager.addProduct({
+  title: 'producto prueba',
+  description: 'Este es un producto prueba',
+  price: 200,
+  thumbnail: 'Sin imagen',
+  code: 'abc123',
+  stock: 25,
+});
+
+console.log(productManager.getProducts());
+
+const productId = 1;
+const product = productManager.getProductById(productId);
+if (product) {
+  console.log('Producto encontrado:', product);
+} else {
+  console.log(`Producto con ID ${productId} no encontrado.`);
+}
+
+const updatedFields = {
+  price: 250,
+  stock: 20,
+};
+productManager.updateProduct(productId, updatedFields);
+
+console.log(productManager.getProducts());
+
+productManager.deleteProduct(productId);
+
+console.log(productManager.getProducts());
