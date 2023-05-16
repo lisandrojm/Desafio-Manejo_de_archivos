6cd686e1b4863a764748135d7bece2a2b3cd5651
lisandrojm <lisandrojm@gmail.com>
2023-05-16 14:09:58 -0300

    require('fs')+class ProductManager

---
+++ ProductManager.js
@@ -0,0 +1,9 @@
+////////////////////////////////////////////////////////////////////////////////
+/* DESAFÍO ENTREGABLE */
+
+const fs = require('fs');
+class ProductManager {
+  constructor(path) {
+    this.path = path;
+  }
+}
